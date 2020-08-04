d961db33e9ef6bef6ccb0deefedff4b19dc1c562
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-08-04 21:22:31 +0200

    add face detection box and signin routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -31,8 +33,31 @@ class App extends React.Component {
 		this.state = {
 			input: 'https://samples.clarifai.com/face-det.jpg',
 			imageUrl: 'https://samples.clarifai.com/face-det.jpg',
+			box: {},
+			route: 'signin',
+			isSignedIn: false,
 		};
 	}
+
+	calculateFaceLocation = data => {
+		const clarifaiFace =
+			data.outputs[0].data.regions[0].region_info.bounding_box;
+
+		const image = document.getElementById('inputimage');
+
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height,
+		};
+	};
+
+	displayFaceBox = box => {
+		this.setState({ box: box });
+	};
 
 	onInputChange = event => {
 		// console.log(event.target.value);
@@ -43,35 +68,49 @@ class App extends React.Component {
 		this.setState({ imageUrl: this.state.input });
 
 		// https://www.clarifai.com/models/face-detection-image-recognition-model-a403429f2ddf4b49b307e318f00e528b-detection
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-			function (response) {
-				// do something with response
-				console.log(
-					response.outputs[0].data.regions[0].region_info.bounding_box
-				);
-				response.outputs[0].data.regions.forEach(region => {
-					console.log(region.region_info.bounding_box);
-				});
-			},
-			function (err) {
-				// there was an error
-				console.log(err);
-			}
-		);
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			.then(response =>
+				this.displayFaceBox(this.calculateFaceLocation(response))
+			)
+			.catch(err => console.log(err));
+	};
+
+	onRouteChange = route => {
+		if (route === 'signout') {
+			this.setState({ isSignedIn: false });
+		} else if (route === 'home') {
+			this.setState({ isSignedIn: true });
+		} else if (route === 'signin') {
+			this.setState({ isSignedIn: true });
+		}
+		this.setState({ route: route });
 	};
 
 	render() {
+		const { isSignedIn, imageUrl, route, box } = this.state;
 		return (
 			<div className="App">
 				<Particles className="particles" params={particlesOptions} />
-				<Navigation />
-				<Logo />
-				<Rank />
-				<ImageLinkForm
-					onInputChange={this.onInputChange}
-					onButtonSubmit={this.onButtonSubmit}
+				<Navigation
+					isSignedIn={isSignedIn}
+					onRouteChange={this.onRouteChange}
 				/>
-				<FaceRecognition imageUrl={this.state.imageUrl} />
+				<Logo />
+				{route === 'home' ? (
+					<div>
+						<Rank />
+						<ImageLinkForm
+							onInputChange={this.onInputChange}
+							onButtonSubmit={this.onButtonSubmit}
+						/>
+						<FaceRecognition imageUrl={imageUrl} box={box} />
+					</div>
+				) : route === 'signin' ? (
+					<Signin onRouteChange={this.onRouteChange} />
+				) : (
+					<Register onRouteChange={this.onRouteChange} />
+				)}
 			</div>
 		);
 	}
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
 	return (
 		<div className="center ma">
-			<img src={imageUrl} alt="detect face"></img>
+			<div className="absolute mt2">
+				<img id="inputimage" src={imageUrl} alt="detect face"></img>
+				<div
+					className="bounding-box"
+					style={{
+						top: box.topRow,
+						right: box.rightCol,
+						bottom: box.bottomRow,
+						left: box.leftCol,
+					}}
+				></div>
+			</div>
 		</div>
 	);
 };
